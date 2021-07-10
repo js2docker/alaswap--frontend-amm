f794fcbff76e6e1f762142eb2888f967ef388e51
js2docker <44265853+js2docker@users.noreply.github.com>
2021-07-10 21:52:28 +0530

    Update hooks.ts

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -91,7 +91,7 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 const BAD_RECIPIENT_ADDRESSES: string[] = [
   '0xBCfCcbde45cE874adCB698cC183deBcF17952812', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F', // v2 router 02
+  '0x24f7C33ae5f77e2A9ECeed7EA858B4ca2fa1B7eC', // v2 router 02
 ]
 
 /**
