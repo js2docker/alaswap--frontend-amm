d1c71c69a97cd04e12e0fc2343ba786a57746d02
js2docker <44265853+js2docker@users.noreply.github.com>
2021-07-12 08:30:36 +0530

    Update index.ts

--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -5,7 +5,7 @@ import V1_FACTORY_ABI from './v1_factory.json'
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0xC07d4604400139108BbdB3076636365A385879eF', // TODO
-  [ChainId.BSCTESTNET]: '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+  [ChainId.BSCTESTNET]: '0x24f7C33ae5f77e2A9ECeed7EA858B4ca2fa1B7eC'
 }
 
 const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
